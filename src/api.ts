1ecbbb471dd3b6f04e855fef9db8131c3181686f
Ildar Karimov <karimovildar2000@gmail.com>
2022-05-13 23:24:12 +0300

    Add axios interceptor of response

--- src/api.ts
+++ src/api.ts
@@ -12,4 +12,30 @@ $api.interceptors.request.use((config) => {
   return config;
 });
 
+$api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status == 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        const res = await axios.get(`${API_URL}/user/refresh`, {
+          withCredentials: true,
+        });
+        localStorage.setItem("token", res.data.accessToken);
+        return $api.request(originalRequest);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    throw error;
+  }
+);
+
 export default $api;
